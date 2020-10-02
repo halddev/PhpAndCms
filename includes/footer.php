0926965d70a0ca08e23aabceaf5339854aaa0ade
Martin Hald <martinhald_4@hotmail.com>
2020-10-02 18:43:48 +0200

    Various smaller fixes

--- includes/footer.php
+++ includes/footer.php
@@ -13,13 +13,13 @@
     <!-- /.container -->
 
     <!-- jQuery -->
-    <script src="js/jquery.js"></script>
+    <script src="/cms/js/jquery.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="/cms/js/bootstrap.min.js"></script>
 
     <!-- own scripts -->
-    <script src="js/scripts.js"></script>
+    <script src="/cms/js/scripts.js"></script>
     
 </body>
 
